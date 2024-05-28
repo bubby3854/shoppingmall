35024a2cde7fd28de8bbad685d0df99179299fce
DESKTOP-4DR04SQ\GG <c1004ds@naver.com>
2024-05-28 18:19:01 +0900

    날짜

--- goshopping/src/main/java/com/example/goshopping/domain/Product.java
+++ goshopping/src/main/java/com/example/goshopping/domain/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,22 +16,28 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 public class Product {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long pNo;
-	private String pName;
-	private String pInfo;
-	private String pPrice;
-	private int pStock;
-	private int pOrder;
-	
-	@Builder
-	public Product(String pName, String pInfo, String pPrice, int pStock, int pOrder) {
-		this.pName = pName;
-		this.pInfo = pInfo;
-		this.pPrice = pPrice;
-		this.pStock = pStock;
-		this.pOrder = pOrder;
-	}
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long pNo;
+    private String pName;
+    private String pInfo;
+    private String pPrice;
+    private int pStock;
+    private int pOrder;
+    private LocalDateTime createdAt;
+    
+    @Builder
+    public Product(String pName, String pInfo, String pPrice, int pStock, int pOrder) {
+        this.pName = pName;
+        this.pInfo = pInfo;
+        this.pPrice = pPrice;
+        this.pStock = pStock;
+        this.pOrder = pOrder;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
